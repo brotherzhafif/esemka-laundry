3fcea7341464260d1737cac9eedb23397eb8455a
Japip <67241834+brotherzhafif@users.noreply.github.com>
2024-05-01 13:52:37 +0700

    Upload Project Lama

---
+++ ESEMKA LAUNDRY/Login.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ESEMKA_LAUNDRY
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.user_txt = new System.Windows.Forms.TextBox();
+            this.pass_txt = new System.Windows.Forms.TextBox();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(-6, -9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 114);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(43, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Esemka Laundry";
+            // 
+            // user_txt
+            // 
+            this.user_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user_txt.Location = new System.Drawing.Point(12, 159);
+            this.user_txt.Name = "user_txt";
+            this.user_txt.Size = new System.Drawing.Size(242, 29);
+            this.user_txt.TabIndex = 1;
+            // 
+            // pass_txt
+            // 
+            this.pass_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass_txt.Location = new System.Drawing.Point(12, 219);
+            this.pass_txt.Name = "pass_txt";
+            this.pass_txt.Size = new System.Drawing.Size(242, 29);
+            this.pass_txt.TabIndex = 2;
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.Silver;
+            this.login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_btn.ForeColor = System.Drawing.SystemColors.Window;
+            this.login_btn.Location = new System.Drawing.Point(12, 263);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(242, 44);
+            this.login_btn.TabIndex = 3;
+            this.login_btn.Text = "Sign In";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Reset Password ?";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(266, 341);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.pass_txt);
+            this.Controls.Add(this.user_txt);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox user_txt;
+        private System.Windows.Forms.TextBox pass_txt;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ESEMKA LAUNDRY/Login.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ESEMKA_LAUNDRY
+{
+    public partial class Login : Form
+    {
+        public static string username_text;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn = new SqlConnection(@"Data Source=BROTHERZHAFIF\SQLEXPRESS;Initial Catalog=esemka_laundry;Integrated Security=True");
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            if (cnn != null)
+            {
+
+                string query = "SELECT * FROM employee WHERE email_employee='" + user_txt.Text + "' AND password_employee='"+ pass_txt.Text +"'";
+
+                if (!string.IsNullOrEmpty(user_txt.Text) && !string.IsNullOrEmpty(pass_txt.Text))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(query, cnn);
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        username_text = dt.Rows[0][3].ToString();
+                        MessageBox.Show("Login Berhasil");
+                        Main main = new Main();
+                        main.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username Atau Password Salah");
+                    }
+                }
+                else
+                {
+                   MessageBox.Show("Tolong Isi Semua Kolom");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Koneksi Gagal");
+            }
+        }
+    }
+}
